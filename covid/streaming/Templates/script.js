9096698cb98b2dbe502eb639e795f91330021aba
Almas M <kramal888@gmail.com>
2020-04-01 13:22:45 +0600

    #dates added to map info

--- covid/streaming/Templates/script.js
+++ covid/streaming/Templates/script.js
@@ -140,7 +140,8 @@ window.onload = function(){
                 '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-danger" style="background:#460000 !important;">Severe Infected <span class="badge badge-primary badge-pill">'+(d.tmp_state_sin)+'</span></button>'+
                 '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-info">Immunized <span class="badge badge-primary badge-pill">'+(d.tmp_state_imm)+'</span></button>'+
                 '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-danger">Death <span class="badge badge-primary badge-pill">'+(d.tmp_state_dea)+'</span></button>'+
-                '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-muted">Start date: '+(mdates[0])+'</button>'+
+                '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-muted">Susceptible <span class="badge badge-primary badge-pill">'+(d.tmp_state_sus)+'</span></button>'+
+                '<button style="min-width: 120px !important; width:190px !important;" type="link" class="btn btn-sm btn-light">Start date: '+(mdates[0])+'</button>'+
             "</div>"+
         "</div>";
 
@@ -154,7 +155,8 @@ window.onload = function(){
             '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-info">Immunized <span class="badge badge-primary badge-pill">'+(d2.tmp_state_imm)+'</span></button>'+
             '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-danger">Death <span class="badge badge-primary badge-pill">'+(d2.tmp_state_dea)+'</span></button>'+
             '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-muted">Susceptible <span class="badge badge-primary badge-pill">'+(d2.tmp_state_sus)+'</span></button>'+
-            '<button style="min-width: 120px !important; width:170px !important;" type="link" class="btn btn-sm btn-muted">Start date: '+(mdates[1])+'</button>'+
+            '<button style="min-width: 120px !important; width:190px !important;" type="link" class="btn btn-sm btn-light">Current date: '+(mdates[1])+'</button>'+
+            
             "</div>"+
         "</div>";
 
